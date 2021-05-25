6059899ad677531a2bee903b13b6a064030a8fe6
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-05-25 00:02:31 -0300

    authenticates with next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
-import Head from "next/head";
 import { AppProps } from "next/app";
+import { Provider as NextAuthProvider } from "next-auth/client";
+
+import { Header } from "../components/Header";
 
 import "../styles/global.scss";
-import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
