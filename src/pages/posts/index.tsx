0fc0ad1879cb2cef9803dec96d7607bed963855e
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-10-05 00:40:21 -0300

    feat: lists post on the frontend

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import { getPrismicClient } from "../../services/prismic";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -15,44 +27,15 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de março de 2021</time>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
 
-            <strong>Lorem ipsum dolor sit amet.</strong>
+              <strong>{post.title}</strong>
 
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Perferendis porro voluptatem nemo dolor pariatur unde ducimus
-              eaque alias quia, maiores explicabo voluptas commodi, voluptatum
-              numquam sapiente reiciendis, cum iure sint!
-            </p>
-          </a>
-
-          <a href="">
-            <time>12 de março de 2021</time>
-
-            <strong>Lorem ipsum dolor sit amet.</strong>
-
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Perferendis porro voluptatem nemo dolor pariatur unde ducimus
-              eaque alias quia, maiores explicabo voluptas commodi, voluptatum
-              numquam sapiente reiciendis, cum iure sint!
-            </p>
-          </a>
-
-          <a href="">
-            <time>12 de março de 2021</time>
-
-            <strong>Lorem ipsum dolor sit amet.</strong>
-
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Perferendis porro voluptatem nemo dolor pariatur unde ducimus
-              eaque alias quia, maiores explicabo voluptas commodi, voluptatum
-              numquam sapiente reiciendis, cum iure sint!
-            </p>
-          </a>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -70,9 +53,27 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
