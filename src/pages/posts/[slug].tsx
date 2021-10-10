e322e11a78878af758b251a9743d36b52467dfb6
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-10-10 09:20:17 -0300

    feat: post preview

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -45,7 +45,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const session = await getSession({ req });
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: "/",
